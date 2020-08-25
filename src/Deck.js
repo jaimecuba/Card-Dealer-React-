820fe55a762da62bcf038092bcf42c3faa51a6e5
JCuba <jim92cuba@gmail.com>
2020-08-25 18:23:48 -0400

    API request succesfull

--- src/Deck.js
+++ src/Deck.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
+import axios from "axios"
+
+const API_URL = "http://deckofcardsapi.com/api/deck/new/shuffle/"
 
 export default class Deck extends Component {
+    constructor(props){
+        super(props);
+        this.state = { deck: null }
+    }
+
+    async componentDidMount(){
+        let deck = await axios.get(API_URL)
+        this.setState({ deck: deck.data })
+    }
+
     render() {
         return (
             <div>
