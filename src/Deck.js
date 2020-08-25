9dbf4d4299069e1c729becd7bddecd4aafaaacc5
JCuba <jim92cuba@gmail.com>
2020-08-25 18:10:11 -0400

    first commit

---
+++ src/Deck.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Deck extends Component {
+    render() {
+        return (
+            <div>
+                <h1> Card Dealer</h1>
+            </div>
+        )
+    }
+}
